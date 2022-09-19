8a852582d2c03bcf2edd653b71a2c35c774d2dbf
moner050 <minhyung0237@gmail.com>
2022-09-19 21:00:24 +0900

    feat: Create JwtTokenProvider

---
+++ src/main/java/com/travelers/jwt/JwtTokenProvider.java
@@ -0,0 +1,11 @@
+package com.travelers.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+
+}
