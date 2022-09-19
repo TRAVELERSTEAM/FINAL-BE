f3553c728239692d556e8ec8553f0c6e65909014
moner050 <minhyung0237@gmail.com>
2022-09-19 17:37:41 +0900

    feat: Add @EnableJpaAuditing Annotation

--- src/main/java/com/travelers/TravelersApplication.java
+++ src/main/java/com/travelers/TravelersApplication.java
@@ -2,8 +2,10 @@ package com.travelers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TravelersApplication {
 
     public static void main(String[] args) {
