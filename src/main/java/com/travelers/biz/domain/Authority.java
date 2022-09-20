fb808005bf3dffa63c90907ce425b18fc897c1b8
moner050 <minhyung0237@gmail.com>
2022-09-21 02:54:50 +0900

    feat: Add isAdmin static method

--- src/main/java/com/travelers/biz/domain/Authority.java
+++ src/main/java/com/travelers/biz/domain/Authority.java
@@ -5,4 +5,8 @@ import lombok.Getter;
 @Getter
 public enum Authority {
     ROLE_ADMIN, ROLE_USER;
+
+    public static boolean isAdmin(Authority target) {
+        return Authority.ROLE_ADMIN == target;
+    }
 }
