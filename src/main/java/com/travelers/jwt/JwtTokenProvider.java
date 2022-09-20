363bf326664c2da6a137a64aefc2c34a82c03918
moner050 <minhyung0237@gmail.com>
2022-09-21 03:00:42 +0900

    feat: Add CreateToken, isValidToken, getClaimsToken, getToken

--- src/main/java/com/travelers/jwt/JwtTokenProvider.java
+++ src/main/java/com/travelers/jwt/JwtTokenProvider.java
@@ -1,5 +1,6 @@
 package com.travelers.jwt;
 
+import io.jsonwebtoken.*;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,7 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.bind.DatatypeConverter;
 import java.util.Base64;
+import java.util.Date;
 
 @Slf4j
 @Component
@@ -21,7 +25,7 @@ public class JwtTokenProvider {
     private String REFRESH_KEY;
 
     private final long ACCESS_TOKEN_TIME = 1 * 60 * 1000L;      // 1분
-    private final long REFRESH_TOKEN_TIME = 60 * 60 * 24 * 7 * 1000L;   // 1주일
+    private final long REFRESH_TOKEN_TIME = 1 * 60 * 3 * 1000L;   // 3분
 
     // 의존성 주입이 이루어진 후 키값을 초기화 하기위해 설정
     @PostConstruct
@@ -34,6 +38,111 @@ public class JwtTokenProvider {
 
         log.info("AFTER : " + SECRET_KEY);
         log.info("AFTER : " + REFRESH_KEY);
+    }
+
+
+    // JWT 토큰 생성
+    public String createAccessToken(String email) {
+        Claims claims = Jwts.claims();
+        claims.put("email", email);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims) // 정보 저장
+                .setIssuedAt(now) // 토큰 발행 시간 정보
+                .setExpiration(new Date(now.getTime() + ACCESS_TOKEN_TIME)) // set Expire Time
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)  // 사용할 암호화 알고리즘과
+                .compact();
+    }
+
+    public String createRefreshToken(String email) {
+        Claims claims = Jwts.claims();
+        claims.put("email", email);
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + REFRESH_TOKEN_TIME);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(SignatureAlgorithm.HS256, REFRESH_KEY)
+                .compact();
+    }
+
+    // Request의 Header에서 token 값을 가져오기
+    public String resolveAccessToken(HttpServletRequest request) {
+        return request.getHeader("ACCESS_TOKEN");
+    }
+
+    public String resolveRefreshToken(HttpServletRequest request) {
+        return request.getHeader("REFRESH_TOKEN");
+    }
+
+    public Claims getClaimsFormToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public Claims getClaimsToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(REFRESH_KEY))
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean isValidAccessToken(String token) {
+        System.out.println("isValidToken is : " +token);
+        try {
+            Claims accessClaims = getClaimsFormToken(token);
+            System.out.println("Access expireTime: " + accessClaims.getExpiration());
+            System.out.println("Access email: " + accessClaims.get("email"));
+            return true;
+        } catch (ExpiredJwtException exception) {
+            System.out.println("Token Expired email : " + exception.getClaims().get("email"));
+            return false;
+        } catch (JwtException exception) {
+            System.out.println("Token Tampered");
+            return false;
+        } catch (NullPointerException exception) {
+            System.out.println("Token is null");
+            return false;
+        }
+    }
+    public boolean isValidRefreshToken(String token) {
+        try {
+            Claims accessClaims = getClaimsToken(token);
+            System.out.println("Access expireTime: " + accessClaims.getExpiration());
+            System.out.println("Access email: " + accessClaims.get("email"));
+            return true;
+        } catch (ExpiredJwtException exception) {
+            System.out.println("Token Expired email : " + exception.getClaims().get("email"));
+            return false;
+        } catch (JwtException exception) {
+            System.out.println("Token Tampered");
+            return false;
+        } catch (NullPointerException exception) {
+            System.out.println("Token is null");
+            return false;
+        }
+    }
+    public boolean isOnlyExpiredToken(String token) {
+        System.out.println("isValidToken is : " +token);
+        try {
+            Claims accessClaims = getClaimsFormToken(token);
+            System.out.println("Access expireTime: " + accessClaims.getExpiration());
+            System.out.println("Access email: " + accessClaims.get("email"));
+            return false;
+        } catch (ExpiredJwtException exception) {
+            System.out.println("Token Expired email : " + exception.getClaims().get("email"));
+            return true;
+        } catch (JwtException exception) {
+            System.out.println("Token Tampered");
+            return false;
+        } catch (NullPointerException exception) {
+            System.out.println("Token is null");
+            return false;
+        }
     }
 
 }
