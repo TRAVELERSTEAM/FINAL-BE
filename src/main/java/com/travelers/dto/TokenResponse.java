ea504fa08c334355021c8a3a213fed2b39587ec8
moner050 <minhyung0237@gmail.com>
2022-09-20 23:14:49 +0900

    feat: Create TokenResponse

---
+++ src/main/java/com/travelers/dto/TokenResponse.java
@@ -0,0 +1,15 @@
+package com.travelers.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+    private String ACCESS_TOKEN;
+    private String REFRESH_TOKEN;
+}
