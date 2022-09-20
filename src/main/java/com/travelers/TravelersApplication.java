8011bb28bd7c28022f98f78d0c53f7ddc1d47951
moner050 <minhyung0237@gmail.com>
2022-09-21 03:35:25 +0900

    feat: Add initData
    시작시 어드민 유저 추가

--- src/main/java/com/travelers/TravelersApplication.java
+++ src/main/java/com/travelers/TravelersApplication.java
@@ -1,15 +1,46 @@
 package com.travelers;
 
+import com.travelers.biz.domain.Authority;
+import com.travelers.biz.domain.Gender;
+import com.travelers.biz.domain.Member;
+import com.travelers.biz.repository.MemberRepository;
+import com.travelers.dto.MemberRequestDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class TravelersApplication {
 
+    @Value("${email}") String email;
+    @Value("${password}") String password;
+
     public static void main(String[] args) {
         SpringApplication.run(TravelersApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner initData(MemberRepository memberRepository, PasswordEncoder passwordEncoder){
+
+        return (arg) -> {
+            Member member = Member.builder()
+                    .email(email)
+                    .username("관리자")
+                    .password(passwordEncoder.encode(password))
+                    .birth("1998-09-16")
+                    .gender(Gender.MALE)
+                    .tel("01055222222")
+                    .hobby("코딩")
+                    .prefer("상관없음")
+                    .build();
+
+            member.changeAuthority(Authority.ROLE_ADMIN);
+            memberRepository.save(member);
+        };
+    }
 }
