f82c6116120fecbd1572c71153d01327827fac65
moner050 <minhyung0237@gmail.com>
2022-09-21 03:01:17 +0900

    feat: Add findByEmail, existsByEmail

--- src/main/java/com/travelers/biz/repository/MemberRepository.java
+++ src/main/java/com/travelers/biz/repository/MemberRepository.java
@@ -3,5 +3,14 @@ package com.travelers.biz.repository;
 import com.travelers.biz.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    // 맴버 찾기(이메일)
+    Optional<Member> findByEmail(String email);
+
+    // 맴버 중복체크(이메일)
+    boolean existsByEmail(String email);
+
 }
