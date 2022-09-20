5e225f7b8548dd17d75a05d497d213905d75043f
moner050 <minhyung0237@gmail.com>
2022-09-20 21:26:10 +0900

    refactor: Change LocalDateTime -> LocalDate & Add Pattern
    년도와 날짜만 나오도록 변경

--- src/main/java/com/travelers/biz/domain/BaseTime.java
+++ src/main/java/com/travelers/biz/domain/BaseTime.java
@@ -5,10 +5,9 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @MappedSuperclass
@@ -17,12 +16,20 @@ public class BaseTime {
 
     @Column(name = "createdAt")
     @CreatedDate
-    private LocalDateTime createdAt;
+    private String createdAt;
 
     @Column(name = "modifiedAt")
     @LastModifiedDate
-    private LocalDateTime modifiedAt;
+    private String modifiedAt;
 
+    @PrePersist
+    public void prePersist(){
+        this.createdAt = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+        this.modifiedAt = this.createdAt;
+    }
 
-
+    @PreUpdate
+    public void preUpdate(){
+        this.modifiedAt = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+    }
 }
