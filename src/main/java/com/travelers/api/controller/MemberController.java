2b328b1ff90382b73bd516d280940523bd2d4f01
moner050 <minhyung0237@gmail.com>
2022-09-22 15:09:57 +0900

    feat: Create myInfo method
    토큰을 받으면 내 정보를 확인하는 메서드 추가

--- src/main/java/com/travelers/api/controller/MemberController.java
+++ src/main/java/com/travelers/api/controller/MemberController.java
@@ -3,12 +3,17 @@ package com.travelers.api.controller;
 import com.travelers.biz.service.MemberService;
 import com.travelers.dto.MemberLoginRequestDto;
 import com.travelers.dto.MemberRequestDto;
+import com.travelers.dto.TokenRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,4 +43,14 @@ public class MemberController {
             return ResponseEntity.badRequest().build();
         }
     }
+
+    // 내 정보 확인
+    @PostMapping("/user/me")
+    public ResponseEntity myInfo(HttpServletRequest req){
+        String accessToken = req.getHeader("accessToken");
+        String refreshToken = req.getHeader("refreshToken");
+
+        return ResponseEntity.ok(memberService.getMemberInfo(accessToken, refreshToken));
+    }
+
 }
