014013d86267806b1ae9e7d2d2c361eeb92fdaeb
moner050 <minhyung0237@gmail.com>
2022-09-23 02:10:12 +0900

    feat: remove findByMemberEmail

--- src/main/java/com/travelers/biz/repository/TokenRepository.java
+++ src/main/java/com/travelers/biz/repository/TokenRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
-    Optional<Token> findByMemberEmail(String email);
     Optional<Token> findById(String tokenId);
 }
