a7def3d2f9416685555d3d545bf3233b41abc8fa
moner050 <minhyung0237@gmail.com>
2022-09-23 02:05:19 +0900

    feat: Add register&login&reissue

--- src/main/java/com/travelers/api/controller/AuthController.java
+++ src/main/java/com/travelers/api/controller/AuthController.java
@@ -1,7 +1,12 @@
 package com.travelers.api.controller;
 
 import com.travelers.biz.service.AuthService;
+import com.travelers.dto.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody MemberRequestDto memberRequestDto) {
+        authService.register(memberRequestDto);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponseDto> login(@RequestBody MemberLoginRequestDto memberLoginRequestDto) {
+        return ResponseEntity.ok(authService.login(memberLoginRequestDto));
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenResponseDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+        return ResponseEntity.ok(authService.reissue(tokenRequestDto));
+    }
 }
