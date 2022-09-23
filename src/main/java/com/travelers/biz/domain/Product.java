b21667c46ab4eb509f07b2abb63b611dcb02b070
keinetwork <keinetwork@gmail.com>
2022-09-23 21:37:03 +0900

    feat: 상품 DB 설계

--- src/main/java/com/travelers/biz/domain/Product.java
+++ src/main/java/com/travelers/biz/domain/Product.java
@@ -1,19 +1,45 @@
 package com.travelers.biz.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author kei
  * @since 2022-09-06
  */
 @Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String title;
+    private Integer price;
+    private String thumbnail;
+    private String target;
+    private String destination;
+    private String theme;
 
-    private String productName;
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<ProductStartDate> startDates = new ArrayList<>();
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<ProductImage> images = new ArrayList<>();
+
+    @Builder
+    public Product(Long id, String title, Integer price, String thumbnail, String target, String destination, String theme, List<ProductStartDate> startDates, List<ProductImage> images) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.target = target;
+        this.destination = destination;
+        this.theme = theme;
+        this.startDates = startDates;
+        this.images = images;
+    }
 }
