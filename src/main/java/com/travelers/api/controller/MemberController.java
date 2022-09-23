29e81893594a707b5ab8b54069a75f003b126dcb
moner050 <minhyung0237@gmail.com>
2022-09-23 23:25:11 +0900

    feat: Add DeleteMyAccount

--- src/main/java/com/travelers/api/controller/MemberController.java
+++ src/main/java/com/travelers/api/controller/MemberController.java
@@ -3,21 +3,29 @@ package com.travelers.api.controller;
 import com.travelers.biz.service.MemberService;
 import com.travelers.dto.MemberResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
 public class MemberController {
 
     private final MemberService memberService;
 
     // 내 정보 확인
-    @PostMapping("/me")
+    @GetMapping("/user/me")
     public ResponseEntity<MemberResponseDto> getMyInfo(){
         return ResponseEntity.ok(memberService.getMyInfo());
     }
+
+    // 회원 탈퇴
+    @DeleteMapping("/user")
+    public ResponseEntity DeleteMyAccount() {
+        memberService.deleteMyAccount();
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
 }
