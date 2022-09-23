824623530a53b176304ba5dd40462475ceb8b4f5
moner050 <minhyung0237@gmail.com>
2022-09-23 17:28:28 +0900

    test: DataJpaTest -> SpringBootTest, Add PasswordEncoder

--- src/test/java/com/travelers/biz/repository/MemberRepositoryTest.java
+++ src/test/java/com/travelers/biz/repository/MemberRepositoryTest.java
@@ -7,47 +7,53 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
-@Transactional
 @Slf4j
+@SpringBootTest
+@Transactional
 class MemberRepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Test
     @DisplayName("회원가입 테스트")
     void 회원가입_테스트() {
 
         Member member = Member.builder()
-                .email("test@naver.com")
+                .email("test1234@naver.com")
                 .username("홍길동")
-                .password("1234")
+                .password(passwordEncoder.encode("1234"))
                 .tel("01022225555")
-                .birth("1998-09-11")
-                .hobby("등산")
-                .prefer("남자끼리, 누구든지")
+                .birth("19980911")
+                .groupTrip("남자끼리")
+                .area("중남미/북미")
+                .theme("문화탐방")
                 .gender(Gender.MALE)
                 .build();
 
         memberRepository.save(member);
 
-        Optional<Member> getMember = memberRepository.findById(1L);
+        Optional<Member> getMember = memberRepository.findByEmail("test1234@naver.com");
 
         log.info(getMember.get().getCreatedAt() + "");
-        log.info(getMember.get().getPrefer() + "");
+        log.info(getMember.get().getTheme() + "");
+        log.info(getMember.get().getPassword() + "");
 
         // 회원가입이 되있나 확인!
         assertThat(getMember.get().getGender()).isEqualTo(Gender.MALE);
         assertThat(getMember.get().getAuthority()).isEqualTo(Authority.ROLE_USER);
-        assertThat(getMember.get().getEmail()).isEqualTo("test@naver.com");
-
+        assertThat(getMember.get().getEmail()).isEqualTo("test1234@naver.com");
     }
+
+
 }
