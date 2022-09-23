aa69d2e8bed82ff4fd8a8e1fc13941cc1bd27e9e
moner050 <minhyung0237@gmail.com>
2022-09-23 23:27:16 +0900

    feat: Add deleteMyAccount, updateAuthority

--- src/main/java/com/travelers/biz/service/MemberService.java
+++ src/main/java/com/travelers/biz/service/MemberService.java
@@ -2,7 +2,10 @@ package com.travelers.biz.service;
 
 import com.travelers.biz.domain.Authority;
 import com.travelers.biz.domain.Member;
+import com.travelers.biz.domain.Token;
 import com.travelers.biz.repository.MemberRepository;
+import com.travelers.biz.repository.TokenRepository;
+import com.travelers.dto.AuthorityResponseDto;
 import com.travelers.dto.MemberResponseDto;
 import com.travelers.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +19,36 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final TokenRepository tokenRepository;
 
+    // USER
     // 현재 SecurityContext 에 있는 유저 정보 가져오기
     @Transactional(readOnly = true)
     public MemberResponseDto getMyInfo() {
         return memberRepository.findById(SecurityUtil.getCurrentMemberId())
                 .map(MemberResponseDto::of)
                 .orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다."));
+    }
+
+    // 회원 탈퇴하기
+    @Transactional
+    public void deleteMyAccount() {
+        Member member = memberRepository.findById(SecurityUtil.getCurrentMemberId())
+                .orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다."));
+        Token token = tokenRepository.findById(String.valueOf(SecurityUtil.getCurrentMemberId()))
+                        .orElseThrow(() -> new RuntimeException("토큰 정보가 없습니다."));
+        memberRepository.delete(member);
+        tokenRepository.delete(token);
+    }
+
+    // ADMIN
+    // 회원 등급 변경
+    @Transactional
+    public void updateAuthority(Long id, AuthorityResponseDto authority){
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다."));
+        member.changeAuthority(authority.toAuthority());
+        memberRepository.save(member);
     }
 
     // 회원 중복 체크(이메일)
