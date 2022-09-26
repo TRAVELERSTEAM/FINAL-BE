7d9cdc6ca506034fc26124507765fc2a58965f07
moner050 <minhyung0237@gmail.com>
2022-09-26 23:38:30 +0900

    feat: Add findByUsernameAndBirthAndGenderAndTelAndEmail

--- src/main/java/com/travelers/biz/repository/MemberRepository.java
+++ src/main/java/com/travelers/biz/repository/MemberRepository.java
@@ -11,8 +11,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     // 맴버 찾기(이메일)
     Optional<Member> findByEmail(String email);
 
-    // 맴버 찾기(이름, 생년월일)
+    // 맴버 찾기(이름, 성별, 생년월일)
     Optional<Member> findByUsernameAndBirthAndGender(String username, String birth, Gender gender);
+
+    // 맴버 찾기(이름, 성별, 생년월일, 전화번호, 이메일)
+    Optional<Member> findByUsernameAndBirthAndGenderAndTelAndEmail(String username, String birth, Gender gender, String tel, String email);
 
     // 맴버 중복체크(이메일)
     boolean existsByEmail(String email);
