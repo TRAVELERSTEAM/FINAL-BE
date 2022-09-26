76466c8dbb3ef59e9bc90836321d6c65e42d4df6
moner050 <minhyung0237@gmail.com>
2022-09-26 14:49:30 +0900

    feat: Create findByUsernameAndBirthAndGender

--- src/main/java/com/travelers/biz/repository/MemberRepository.java
+++ src/main/java/com/travelers/biz/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.travelers.biz.repository;
 
+import com.travelers.biz.domain.Gender;
 import com.travelers.biz.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,6 +10,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     // 맴버 찾기(이메일)
     Optional<Member> findByEmail(String email);
+
+    // 맴버 찾기(이름, 생년월일)
+    Optional<Member> findByUsernameAndBirthAndGender(String username, String birth, Gender gender);
 
     // 맴버 중복체크(이메일)
     boolean existsByEmail(String email);
