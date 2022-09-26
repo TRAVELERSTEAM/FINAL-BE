539a665916a150f8238905b857b842818265ea96
moner050 <minhyung0237@gmail.com>
2022-09-26 14:47:04 +0900

    feat: Create EmailController

---
+++ src/main/java/com/travelers/api/controller/EmailController.java
@@ -0,0 +1,42 @@
+package com.travelers.api.controller;
+
+import com.travelers.biz.service.EmailService;
+import com.travelers.biz.service.MemberService;
+import com.travelers.dto.EmailRequestDto;
+import com.travelers.dto.MemberResetPasswordRequestDto;
+import com.travelers.util.RedisUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final MemberService memberService;
+    private final EmailService emailService;
+    private final RedisUtil redisUtil;
+
+    private final long EMAIL_EXPIRE_TIME = 60 * 5L;
+
+    // 회원가입 이메일 인증번호 발송
+    @PostMapping("/verify")
+    public ResponseEntity<Objects> joinMail(@RequestBody EmailRequestDto emailRequestDto) {
+        String key = emailService.joinMail(emailRequestDto.getEmail());
+        redisUtil.setDataExpire(key, emailRequestDto.getEmail(), EMAIL_EXPIRE_TIME);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 회원가입 이메일 인증번호 확인
+    @GetMapping("/verify/{email}/{key}")
+    public ResponseEntity<Objects> verifyEmailKey(@PathVariable String email, @PathVariable String key){
+        if(emailService.verifyKey(email, key)) return new ResponseEntity(HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+
+}
