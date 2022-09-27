3edc87b8f7fa061ccfee258acc23c37927494d83
moner050 <minhyung0237@gmail.com>
2022-09-27 15:18:04 +0900

    feat: add allMember

--- src/main/java/com/travelers/admin/controller/AdminMemberController.java
+++ src/main/java/com/travelers/admin/controller/AdminMemberController.java
@@ -1,13 +1,14 @@
 package com.travelers.admin.controller;
 
-import com.travelers.biz.service.AuthService;
 import com.travelers.biz.service.MemberService;
 import com.travelers.dto.AuthorityResponseDto;
+import com.travelers.dto.MemberResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Objects;
 
 @RestController
@@ -16,7 +17,12 @@ import java.util.Objects;
 public class AdminMemberController {
 
     private final MemberService memberService;
-    private final AuthService authService;
+
+    // 전체 회원 목록 출력
+    @GetMapping("/members")
+    public ResponseEntity<List<MemberResponseDto>> allMember(){
+        return ResponseEntity.ok(memberService.showAllMember());
+    }
 
     // 회원 등급 변경
     @PutMapping("/member/{id}")
