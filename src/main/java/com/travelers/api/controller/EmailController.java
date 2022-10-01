6694ce637a21033ece6271907e341a78d85ad23e
moner050 <minhyung0237@gmail.com>
2022-10-01 18:07:39 +0900

    feat: modify verifyEmailKey
    회원가입 인증번호 확인 시 해당 키갑의 유효기간 바꾸고 상태코드 리턴시키게 수정

--- src/main/java/com/travelers/api/controller/EmailController.java
+++ src/main/java/com/travelers/api/controller/EmailController.java
@@ -16,8 +16,6 @@ import java.util.Objects;
 @RestController
 @RequiredArgsConstructor
 public class EmailController {
-
-    private final MemberService memberService;
     private final EmailService emailService;
     private final RedisUtil redisUtil;
 
@@ -34,8 +32,12 @@ public class EmailController {
     // 회원가입 이메일 인증번호 확인
     @GetMapping("/verify/{email}/{key}")
     public ResponseEntity<Objects> verifyEmailKey(@PathVariable String email, @PathVariable String key){
-        if(emailService.verifyKey(email, key)) return new ResponseEntity<>(HttpStatus.OK);
+        if(emailService.verifyKey(email, key)) {
+            if(emailService.changeExpireKey(email, key)){
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+            else return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
         else return new ResponseEntity<>(HttpStatus.FORBIDDEN);
     }
-
 }
