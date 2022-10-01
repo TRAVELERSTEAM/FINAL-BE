882c87a7186b8e6d1c9599b1e48bf66cf9addcb2
jungsunghun <jsh951227@gmail.com>
2022-10-02 02:26:59 +0900

    test: notify 단건 조회 테스트

--- src/test/java/com/travelers/biz/repository/NotifyRepositoryTest.java
+++ src/test/java/com/travelers/biz/repository/NotifyRepositoryTest.java
@@ -9,12 +9,17 @@ import com.travelers.dto.PagingResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static com.travelers.biz.domain.notify.QNotify.notify;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -62,10 +67,10 @@ class NotifyRepositoryTest {
     @Test
     @DisplayName("공지사항 목록 출력")
     void select_notice_list() {
-        PageRequest pagingCondition = PageRequest.of(0, 7, Sort.by("createdAt").descending());
+        final PageRequest pagingCondition = PageRequest.of(0, 7, Sort.by("createdAt").descending());
 
-        PagingResponse<NotifyResponse.SimpleInfo> noticeList = notifyRepository.findSimpleList(NotifyType.NOTICE, pagingCondition);
-        List<NotifyResponse.SimpleInfo> contents = noticeList.getContents();
+        final PagingResponse<NotifyResponse.SimpleInfo> noticeList = notifyRepository.findSimpleList(NotifyType.NOTICE, pagingCondition);
+        final List<NotifyResponse.SimpleInfo> contents = noticeList.getContents();
 
         then(contents.size()).isSameAs(7);
         then(noticeList.getCurrentPage()).isSameAs(1);
@@ -82,5 +87,27 @@ class NotifyRepositoryTest {
                         "입금 계좌 안내",
                         "백신 및 안내 규정",
                         "배고프다");
+    }
+
+    @ParameterizedTest
+    @MethodSource("aroundTest")
+    @DisplayName("공지사항 한 건 출력 시 이전과 다음 게시글이 있으면 제목과 번호를 가져와야 한다.")
+    void select_one(final Long id, final String title, final String[] aroundTitle) {
+        NotifyResponse.DetailInfo detail = notifyRepository.findDetail(id, NotifyType.NOTICE);
+
+        then(detail.getId()).isSameAs(id);
+        then(detail.getTitle()).isEqualTo(title);
+
+        then(detail.getAroundTitles())
+                .extracting("title", String.class)
+                .containsExactly(aroundTitle);
+    }
+
+    private static Stream<Arguments> aroundTest(){
+        return Stream.of(
+                Arguments.of(1L, "여행 시 규칙", new String[]{"환불 규정"}),
+                Arguments.of(2L, "환불 규정", new String[]{"여행 시 규칙", "안전 수칙"}),
+                Arguments.of( 3L, "안전 수칙", new String[]{"환불 규정", "전화 상담에 대한 안내문"})
+        );
     }
 }
