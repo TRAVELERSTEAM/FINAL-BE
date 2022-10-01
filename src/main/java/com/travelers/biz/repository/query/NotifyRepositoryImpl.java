9c258756ffb7a13953e20caf55990a0e2a0167c1
jungsunghun <jsh951227@gmail.com>
2022-10-01 22:48:43 +0900

    feat: 리턴 타입 변경

--- src/main/java/com/travelers/biz/repository/query/NotifyRepositoryImpl.java
+++ src/main/java/com/travelers/biz/repository/query/NotifyRepositoryImpl.java
@@ -1,4 +1,59 @@
 package com.travelers.biz.repository.query;
 
-public class NotifyRepositoryImpl implements NotifyRepositoryQuery{
+import com.querydsl.core.types.ConstructorExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.travelers.biz.domain.notify.NotifyType;
+import com.travelers.biz.domain.notify.QNotify;
+import com.travelers.biz.repository.query.config.QuerydslSupports;
+import com.travelers.dto.NotifyResponse;
+import com.travelers.dto.PagingResponse;
+import com.travelers.dto.QNotifyResponse_SimpleInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import javax.persistence.EntityManager;
+
+import static com.travelers.biz.domain.QMember.member;
+import static com.travelers.biz.domain.notify.QNotify.notify;
+
+public class NotifyRepositoryImpl extends QuerydslSupports implements NotifyRepositoryQuery {
+
+    private static final QNotify N = notify;
+
+    public NotifyRepositoryImpl(EntityManager em) {
+        super(notify, em);
+    }
+
+    private <T> JPAQuery<T> selectFromWhere(final NotifyType notifyType, ConstructorExpression<T> dtoResponse) {
+        return select(dtoResponse)
+                .from(N)
+                .where(N.notifyType.eq(notifyType));
+    }
+
+    private JPAQuery<Long> countQuery(final NotifyType type) {
+        return select(N.count())
+                .from(N)
+                .where(N.notifyType.eq(type));
+    }
+
+    @Override
+    public PagingResponse<NotifyResponse.SimpleInfo> findSimpleList(final NotifyType notifyType, final Pageable pageable) {
+        return PagingResponse.from(
+                applyPagination(pageable,
+                        () -> selectFromWhere(notifyType,
+                                new QNotifyResponse_SimpleInfo(
+                                        N.sequence,
+                                        member.username,
+                                        N.content,
+                                        N.createdAt)
+                        )
+                                .join(N.writer, member)
+                                .offset(pageable.getOffset())
+                                .limit(pageable.getPageSize())
+                                .orderBy(N.createdAt.desc()),
+                        countQuery(notifyType)
+                )
+        );
+    }
+
 }
--- src/main/java/com/travelers/biz/repository/query/NotifyRepositoryQuery.java
+++ src/main/java/com/travelers/biz/repository/query/NotifyRepositoryQuery.java
@@ -1,4 +1,10 @@
 package com.travelers.biz.repository.query;
 
+import com.travelers.biz.domain.notify.NotifyType;
+import com.travelers.dto.NotifyResponse;
+import com.travelers.dto.PagingResponse;
+import org.springframework.data.domain.Pageable;
+
 public interface NotifyRepositoryQuery {
+    PagingResponse<NotifyResponse.SimpleInfo> findSimpleList(final NotifyType notifyType, final Pageable pageable);
 }
