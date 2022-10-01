c9fadd369e205983e38765984bf84efeb8929577
jungsunghun <jsh951227@gmail.com>
2022-10-01 20:13:35 +0900

    feat: notifyRepository querydsl 적용

--- src/main/java/com/travelers/biz/repository/NotifyRepository.java
+++ src/main/java/com/travelers/biz/repository/NotifyRepository.java
@@ -1,7 +1,8 @@
 package com.travelers.biz.repository;
 
 import com.travelers.biz.domain.notify.Notify;
+import com.travelers.biz.repository.query.NotifyRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotifyRepository extends JpaRepository<Notify, Long> {
+public interface NotifyRepository extends JpaRepository<Notify, Long>, NotifyRepositoryQuery {
 }
