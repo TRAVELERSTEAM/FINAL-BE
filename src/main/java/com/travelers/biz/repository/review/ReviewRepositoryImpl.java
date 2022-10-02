2b84f2d7dc46706245bae0a5d692770437c47488
jungsunghun <jsh951227@gmail.com>
2022-10-02 20:51:54 +0900

    feat: 상품별 리뷰 리스트 출력 로직 작성

--- src/main/java/com/travelers/biz/repository/review/ReviewRepositoryImpl.java
+++ src/main/java/com/travelers/biz/repository/review/ReviewRepositoryImpl.java
@@ -1,12 +1,45 @@
 package com.travelers.biz.repository.review;
 
+import com.travelers.biz.domain.QReview;
 import com.travelers.biz.repository.notify.config.QuerydslSupports;
+import com.travelers.dto.QReviewResponse_SimpleInfo;
+import com.travelers.dto.ReviewResponse;
+import com.travelers.dto.paging.PagingCorrespondence;
+import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
 
-public class ReviewRepositoryImpl extends QuerydslSupports implements ReviewRepositoryQuery{
+import static com.travelers.biz.domain.QMember.member;
+import static com.travelers.biz.domain.QReview.review;
+
+public class ReviewRepositoryImpl extends QuerydslSupports implements ReviewRepositoryQuery {
+
+    private static final QReview R = review;
 
     public ReviewRepositoryImpl(EntityManager em) {
         super(em);
+    }
+
+    public PagingCorrespondence.Response<ReviewResponse.SimpleInfo> findSimpleList(final Long productId, final Pageable pageable) {
+        return PagingCorrespondence.Response.from(
+                applyPagination(pageable,
+                        () -> select(
+                                new QReviewResponse_SimpleInfo(
+                                        R.id,
+                                        member.username,
+                                        R.content,
+                                        R.createdAt
+                                ))
+                                .from(R)
+                                .join(R.writer, member)
+                                .where(R.placeToTravel.id.eq(productId))
+                                .offset(pageable.getOffset())
+                                .limit(pageable.getPageSize())
+                                .orderBy(R.id.desc()),
+                        select(R.count())
+                                .from(R)
+                                .where(R.placeToTravel.id.eq(productId))
+                )
+        );
     }
 }
--- src/main/java/com/travelers/biz/repository/review/ReviewRepositoryQuery.java
+++ src/main/java/com/travelers/biz/repository/review/ReviewRepositoryQuery.java
@@ -1,4 +1,10 @@
 package com.travelers.biz.repository.review;
 
+import com.travelers.dto.ReviewResponse;
+import com.travelers.dto.paging.PagingCorrespondence;
+import org.springframework.data.domain.Pageable;
+
 public interface ReviewRepositoryQuery {
+
+    PagingCorrespondence.Response<ReviewResponse.SimpleInfo> findSimpleList(final Long productId, final Pageable pageable);
 }
