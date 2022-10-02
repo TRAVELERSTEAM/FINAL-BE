ded0b9f0b2a3329ad69483fea05d42a0ecea6716
jungsunghun <jsh951227@gmail.com>
2022-10-03 06:38:03 +0900

    feat: 리뷰 등록 코드 작성

--- src/main/java/com/travelers/api/controller/ReviewController.java
+++ src/main/java/com/travelers/api/controller/ReviewController.java
@@ -1,9 +1,12 @@
 package com.travelers.api.controller;
 
 import com.travelers.biz.service.ReviewService;
+import com.travelers.dto.BoardRequest;
 import com.travelers.dto.ReviewResponse;
 import com.travelers.dto.paging.PagingCorrespondence;
+import com.travelers.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +17,7 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
-    @GetMapping()
+    @GetMapping
     public ResponseEntity<PagingCorrespondence.Response<ReviewResponse.SimpleInfo>> showAll(
             final Long productId,
             final PagingCorrespondence.Request pagingInfo
@@ -26,10 +29,23 @@ public class ReviewController {
 
     @GetMapping("/{reviewId}")
     public ResponseEntity<ReviewResponse.DetailInfo> showOne(
-            final Long reviewId
+            @PathVariable final Long reviewId
     ) {
         return ResponseEntity.ok(
                 reviewService.showReview(reviewId)
         );
+    }
+
+    @PostMapping("/{productId}")
+    public ResponseEntity<Void> create(
+            @PathVariable final Long productId,
+            @RequestBody final BoardRequest.Write write
+    ) {
+        reviewService.write(currentMemberId(), productId, write);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    private Long currentMemberId() {
+        return SecurityUtil.getCurrentMemberId();
     }
 }
