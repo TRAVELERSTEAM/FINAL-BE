d424a33de1024ca9ca2ac1f32edbcedeeab75fb6
jungsunghun <jsh951227@gmail.com>
2022-10-02 19:10:41 +0900

    feat: 회원이 다녀온 여행지 정보를 저장하는 엔티티 추가

---
+++ src/main/java/com/travelers/biz/domain/TravelPlace.java
@@ -0,0 +1,29 @@
+package com.travelers.biz.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TravelPlace {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public TravelPlace(final Member member, final Product product) {
+        this.member = member;
+        this.product = product;
+    }
+}
