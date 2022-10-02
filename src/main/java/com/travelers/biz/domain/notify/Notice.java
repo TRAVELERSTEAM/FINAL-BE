20c1b30f82a57dc86bc2e8a9bba2ba93e3497e29
jungsunghun <jsh951227@gmail.com>
2022-10-02 18:00:07 +0900

    feat: 내부 구조 수정

--- src/main/java/com/travelers/biz/domain/notify/Notice.java
+++ src/main/java/com/travelers/biz/domain/notify/Notice.java
@@ -2,8 +2,8 @@ package com.travelers.biz.domain.notify;
 
 import com.travelers.biz.domain.image.Image;
 import com.travelers.biz.domain.Member;
+import com.travelers.biz.domain.image.NotifyImage;
 import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.DiscriminatorValue;
@@ -20,16 +20,6 @@ public class Notice extends Notify{
 
     public static Notice create(final Member writer, final String title, final String content){
         return new Notice(writer, title, content);
-    }
-
-    @Override
-    public void addImage(final Image image) {
-        super.getImages().add(image);
-    }
-
-    @Override
-    protected void edit(final String title, final String content) {
-        super.edit(title, content);
     }
 
     @Override
--- src/main/java/com/travelers/biz/domain/notify/Notify.java
+++ src/main/java/com/travelers/biz/domain/notify/Notify.java
@@ -3,6 +3,7 @@ package com.travelers.biz.domain.notify;
 import com.travelers.biz.domain.base.BaseContent;
 import com.travelers.biz.domain.image.Image;
 import com.travelers.biz.domain.Member;
+import com.travelers.biz.domain.image.NotifyImage;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,7 +26,7 @@ public abstract class Notify extends BaseContent {
     protected Long sequence;
 
     @OneToMany(mappedBy = "notify", cascade = CascadeType.ALL)
-    private final List<Image> images = new ArrayList<>();
+    private final List<NotifyImage> images = new ArrayList<>();
 
     @Enumerated(EnumType.STRING)
     private NotifyType notifyType;
@@ -35,5 +36,7 @@ public abstract class Notify extends BaseContent {
         this.notifyType = notifyType;
     }
 
-    abstract public void addImage(final Image image);
+    public void addImage(final NotifyImage image) {
+        images.add(image);
+    }
 }
