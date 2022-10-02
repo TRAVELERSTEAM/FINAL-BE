d30b7e5f596e71780649a0d9fe3ae14cfa122323
jungsunghun <jsh951227@gmail.com>
2022-10-02 18:58:06 +0900

    feat: review entity 추가

---
+++ src/main/java/com/travelers/biz/domain/Review.java
@@ -0,0 +1,43 @@
+package com.travelers.biz.domain;
+
+import com.travelers.biz.domain.base.BaseContent;
+import com.travelers.biz.domain.image.ReviewImage;
+import com.travelers.biz.domain.notify.Notice;
+import io.lettuce.core.dynamic.annotation.CommandNaming;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review extends BaseContent {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product placeToTravel;
+
+    private Review(final Member writer, final Product placeToTravel, final String title, final String content){
+        super(writer, title, content);
+        this.placeToTravel = placeToTravel;
+    }
+
+    public static Review create(final Member writer, final Product placeToTravel, final String title, final String content){
+        return new Review(writer, placeToTravel, title, content);
+    }
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL)
+    private final List<ReviewImage> images = new ArrayList<>();
+
+    public void addImage(final ReviewImage reviewImage){
+        images.add(reviewImage);
+    }
+
+}
