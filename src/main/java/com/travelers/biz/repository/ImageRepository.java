de08cc08e3b415cb286c5dd4b92a97c413f0e4ad
jungsunghun <jsh951227@gmail.com>
2022-10-02 19:01:41 +0900

    feat: notifyimage 조회 로직 추가

--- src/main/java/com/travelers/biz/repository/ImageRepository.java
+++ src/main/java/com/travelers/biz/repository/ImageRepository.java
@@ -2,6 +2,13 @@ package com.travelers.biz.repository;
 
 import com.travelers.biz.domain.image.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    @Query(" select i from NotifyImage i where i.notify.id = :notifyId")
+    List<Image> findAllByNotifyId(@Param("notifyId") final Long notifyId);
 }
