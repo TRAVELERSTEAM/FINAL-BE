1c62d649584c0e01ef69669c26b1247050b109b1
moner050 <minhyung0237@gmail.com>
2022-10-03 00:01:51 +0900

    feat: Create GlobalExceptionHandler

---
+++ src/main/java/com/travelers/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.travelers.Exception;
+
+import com.travelers.dto.ErrorResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static com.travelers.Exception.ErrorCode.DUPLICATE_RESOURCE;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { ConstraintViolationException.class, DataIntegrityViolationException.class})
+    protected ResponseEntity<ErrorResponseDto> handleDataException() {
+        log.error("handleDataException throw Exception : {}", DUPLICATE_RESOURCE);
+        return ErrorResponseDto.toResponseEntity(DUPLICATE_RESOURCE);
+    }
+
+    @ExceptionHandler(value = { TravelersException.class })
+    protected ResponseEntity<ErrorResponseDto> handleCustomException(TravelersException e) {
+        log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
+        return ErrorResponseDto.toResponseEntity(e.getErrorCode());
+    }
+}
