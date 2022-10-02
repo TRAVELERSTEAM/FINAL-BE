f2fa532b55b8694a8e01699bd6b2718e04b1e587
jungsunghun <jsh951227@gmail.com>
2022-10-03 07:12:19 +0900

    feat: 여행지를 다녀온 회원에 대한 체크 로직

--- src/main/java/com/travelers/biz/repository/TravelPlaceRepository.java
+++ src/main/java/com/travelers/biz/repository/TravelPlaceRepository.java
@@ -2,6 +2,13 @@ package com.travelers.biz.repository;
 
 import com.travelers.biz.domain.TravelPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface TravelPlaceRepository extends JpaRepository<TravelPlace, Long> {
+
+    @Query("select tp from TravelPlace tp where tp.member.id = :memberId and tp.product.id = :productId")
+    boolean existsPlace(@Param("memberId") final Long memberId, @Param("productId") final Long productId);
 }
