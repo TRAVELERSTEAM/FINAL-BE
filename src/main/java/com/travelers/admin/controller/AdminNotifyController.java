1cf0764940cb5c78a265d5d22ff3ace198b0f5e4
jungsunghun <jsh951227@gmail.com>
2022-10-03 03:13:49 +0900

    feat: adminNotifyController url 위치 변경

--- src/main/java/com/travelers/admin/controller/AdminNotifyController.java
+++ src/main/java/com/travelers/admin/controller/AdminNotifyController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/notify")
+@RequestMapping("/admin/notify")
 public class AdminNotifyController {
 
     private final NotifyService notifyService;
