e14327714d65eb609e15ee46e940390b5698531f
jungsunghun <jsh951227@gmail.com>
2022-10-03 01:55:09 +0900

    feat: 여행 후기 단건 조회 로직 추가

--- src/main/java/com/travelers/biz/repository/review/ReviewRepositoryImpl.java
+++ src/main/java/com/travelers/biz/repository/review/ReviewRepositoryImpl.java
@@ -1,13 +1,20 @@
 package com.travelers.biz.repository.review;
 
+import com.querydsl.jpa.JPAExpressions;
 import com.travelers.biz.domain.QReview;
 import com.travelers.biz.repository.notify.config.QuerydslSupports;
+import com.travelers.dto.QReviewResponse_AroundTitle;
+import com.travelers.dto.QReviewResponse_DetailInfo;
 import com.travelers.dto.QReviewResponse_SimpleInfo;
 import com.travelers.dto.ReviewResponse;
 import com.travelers.dto.paging.PagingCorrespondence;
 import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import static com.travelers.biz.domain.QMember.member;
 import static com.travelers.biz.domain.QReview.review;
@@ -42,4 +49,94 @@ public class ReviewRepositoryImpl extends QuerydslSupports implements ReviewRepo
                 )
         );
     }
+
+    @Override
+    public Optional<ReviewResponse.DetailInfo> findDetailInfo(final Long reviewId) {
+        final Optional<ReviewResponse.DetailInfo> detailInfo =
+                Optional.ofNullable(select(
+                        new QReviewResponse_DetailInfo(
+                                R.id,
+                                R.placeToTravel.id,
+                                member.username,
+                                R.title,
+                                R.content,
+                                R.createdAt
+                        ))
+                        .from(R)
+                        .join(R.writer, member)
+                        .where(R.id.eq(reviewId))
+                        .fetchFirst());
+
+
+        List<ReviewResponse.AroundTitle> aroundTitles = new ArrayList<>();
+        addPrev(aroundTitles, reviewId);
+        addNext(aroundTitles, reviewId);
+
+        detailInfo.ifPresent(
+                e -> e.addAroundTitles(aroundTitles)
+        );
+
+        return detailInfo;
+    }
+
+    private void addPrev(List<ReviewResponse.AroundTitle> aroundTitles, final Long reviewId) {
+
+        ReviewResponse.AroundTitle prev = getPrev(reviewId);
+        if (prev != null) {
+            aroundTitles.add(prev);
+        }
+    }
+
+    private void addNext(List<ReviewResponse.AroundTitle> aroundTitles, final Long reviewId) {
+
+        ReviewResponse.AroundTitle next = getNext(reviewId);
+        if (next != null) {
+            aroundTitles.add(next);
+        }
+    }
+
+    private ReviewResponse.AroundTitle getPrev(final Long reviewId) {
+        QReview sub = new QReview("reviewSub");
+        Long productId = select(review.placeToTravel.id)
+                .from(review)
+                .where(review.id.eq(reviewId))
+                .fetchOne();
+
+        return select(new QReviewResponse_AroundTitle(
+                review.id,
+                review.title
+        ))
+                .from(review)
+                .where(review.id.eq(
+                        JPAExpressions.select(sub.id.max())
+                                .from(sub)
+                                .where(sub.id.lt(reviewId),
+                                        sub.placeToTravel.id.eq(productId)
+                                )))
+                .fetchFirst();
+    }
+
+    private ReviewResponse.AroundTitle getNext(final Long reviewId) {
+        QReview sub = new QReview("reviewSub");
+        Long productId = select(review.placeToTravel.id)
+                .from(review)
+                .where(review.id.eq(reviewId))
+                .fetchOne();
+
+        return select(new QReviewResponse_AroundTitle(
+                review.id,
+                review.title
+        ))
+                .from(review)
+                .where(review.id.eq(
+                        JPAExpressions.select(sub.id.min())
+                                .from(sub)
+                                .where(sub.id.gt(reviewId),
+                                        sub.placeToTravel.id.eq(productId)
+                                )))
+                .fetchFirst();
+
+    }
+
+
 }
--- src/main/java/com/travelers/biz/repository/review/ReviewRepositoryQuery.java
+++ src/main/java/com/travelers/biz/repository/review/ReviewRepositoryQuery.java
@@ -4,7 +4,11 @@ import com.travelers.dto.ReviewResponse;
 import com.travelers.dto.paging.PagingCorrespondence;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 public interface ReviewRepositoryQuery {
 
     PagingCorrespondence.Response<ReviewResponse.SimpleInfo> findSimpleList(final Long productId, final Pageable pageable);
+
+    Optional<ReviewResponse.DetailInfo> findDetailInfo(final Long reviewId);
 }
