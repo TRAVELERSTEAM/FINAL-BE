c79a70be1770233ed477548333cd288ea8719d16
jungsunghun <jsh951227@gmail.com>
2022-10-03 20:39:16 +0900

    feat: 단건 조회 로직 추가

--- src/main/java/com/travelers/biz/repository/qna/QnaRepositoryImpl.java
+++ src/main/java/com/travelers/biz/repository/qna/QnaRepositoryImpl.java
@@ -1,15 +1,28 @@
 package com.travelers.biz.repository.qna;
 
+import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.dsl.StringPath;
+import com.querydsl.jpa.JPAExpressions;
+import com.travelers.biz.domain.QMember;
 import com.travelers.biz.domain.QQna;
+import com.travelers.biz.domain.QReply;
 import com.travelers.biz.repository.notify.config.QuerydslSupports;
-import com.travelers.dto.QQnaResponse_SimpleList;
+import com.travelers.dto.QQnaResponse_DetailInfo;
+import com.travelers.dto.QQnaResponse_ReplyResponse;
+import com.travelers.dto.QQnaResponse_SimpleInfo;
 import com.travelers.dto.QnaResponse;
 import com.travelers.dto.paging.PagingCorrespondence;
 import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import static com.travelers.biz.domain.QMember.member;
 import static com.travelers.biz.domain.QQna.qna;
+import static com.travelers.biz.domain.QReply.reply;
 
 public class QnaRepositoryImpl extends QuerydslSupports implements QnaRepositoryQuery {
 
@@ -17,20 +30,59 @@ public class QnaRepositoryImpl extends QuerydslSupports implements QnaRepository
         super(em);
     }
 
-    public PagingCorrespondence.Response<QnaResponse.SimpleList> findSimpleList(final Pageable pageable) {
+    @Override
+    public PagingCorrespondence.Response<QnaResponse.SimpleInfo> findSimpleList(
+            final Long memberId,
+            final Pageable pageable
+    ) {
         return PagingCorrespondence.Response.from(
                 applyPagination(pageable,
-                        () -> select(new QQnaResponse_SimpleList(
+                        () -> select(new QQnaResponse_SimpleInfo(
                                 qna.id,
                                 qna.status,
                                 qna.title,
                                 qna.createdAt
                         ))
                                 .from(qna)
+                                .where(qna.writer.id.eq(memberId))
                                 .offset(pageable.getOffset())
                                 .limit(pageable.getPageSize())
                                 .orderBy(qna.id.desc()),
                         select(qna.count()).from(qna)
                 ));
     }
+
+    @Override
+    public Optional<QnaResponse.DetailInfo> findDetailInfo(final Long qnaId) {
+        Optional<QnaResponse.DetailInfo> detailInfo = Optional.ofNullable(
+                select(new QQnaResponse_DetailInfo(
+                        qna.id,
+                        qna.status,
+                        qna.title,
+                        qna.content,
+                        qna.createdAt
+                ))
+                        .from(qna)
+                        .fetchOne()
+        );
+        detailInfo.ifPresent(setReplyList(qnaId));
+
+        return detailInfo;
+    }
+
+    private Consumer<QnaResponse.DetailInfo> setReplyList(final Long qnaId) {
+        return e -> e.addReplyList(
+                select(new QQnaResponse_ReplyResponse(
+                        reply.id,
+                        member.username,
+                        reply.content,
+                        reply.createdAt
+                ))
+                        .from(reply)
+                        .where(reply.qna.id.eq(qnaId))
+                        .join(reply.writer, member)
+                        .fetch()
+        );
+    }
+
 }
--- src/main/java/com/travelers/biz/repository/qna/QnaRepositoryQuery.java
+++ src/main/java/com/travelers/biz/repository/qna/QnaRepositoryQuery.java
@@ -4,6 +4,9 @@ import com.travelers.dto.QnaResponse;
 import com.travelers.dto.paging.PagingCorrespondence;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 public interface QnaRepositoryQuery {
-    PagingCorrespondence.Response<QnaResponse.SimpleList> findSimpleList(final Pageable pageable);
+    PagingCorrespondence.Response<QnaResponse.SimpleInfo> findSimpleList(final Long memberId, final Pageable pageable);
+    Optional<QnaResponse.DetailInfo> findDetailInfo(final Long qnaId);
 }
