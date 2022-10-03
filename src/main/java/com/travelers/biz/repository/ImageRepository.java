b72893be1bd3b384a24cc5cbeaa92bb0917eabb5
jungsunghun <jsh951227@gmail.com>
2022-10-03 23:55:38 +0900

    fix: 잘못된 alias 사용 수정

--- src/main/java/com/travelers/biz/repository/ImageRepository.java
+++ src/main/java/com/travelers/biz/repository/ImageRepository.java
@@ -11,4 +11,10 @@ public interface ImageRepository extends JpaRepository<Image, Long> {
 
     @Query(" select i from NotifyImage i where i.notify.id = :notifyId")
     List<Image> findAllByNotifyId(@Param("notifyId") final Long notifyId);
+
+    @Query(" select i from ReviewImage i where i.review.id = :reviewId")
+    List<Image> findAllByReviewId(@Param("reviewId") final Long reviewId);
+
+    @Query(" select i from QnaImage i where i.qna.id = :qnaId")
+    List<Image> findAllByQnaId(@Param("qnaId") final Long qnaId);
 }
