b5143ff4442cf7b50b0199a658a48b5610c0b2e6
jungsunghun <jsh951227@gmail.com>
2022-10-03 23:18:13 +0900

    -

--- src/main/java/com/travelers/biz/domain/Qna.java
+++ src/main/java/com/travelers/biz/domain/Qna.java
@@ -1,6 +1,8 @@
 package com.travelers.biz.domain;
 
 import com.travelers.biz.domain.base.BaseContent;
+import com.travelers.biz.domain.image.QnaImage;
+import com.travelers.biz.domain.image.ReviewImage;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,8 +19,7 @@ public class Qna extends BaseContent {
 
     @RequiredArgsConstructor
     public enum Status{
-        UN_RESPONSE,
-        RESPONSE,;
+        UN_RESPONSE, RESPONSE,;
 
         public Status change(final Member member){
             return member.getAuthority() == Authority.ROLE_ADMIN ? RESPONSE : UN_RESPONSE;
@@ -30,19 +31,27 @@ public class Qna extends BaseContent {
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    private Status status = Status.UN_RESPONSE;
+    private Status status;
 
     private int replyCnt;
+
+    @OneToMany(mappedBy = "qna", cascade = CascadeType.ALL)
+    private final List<QnaImage> images = new ArrayList<>();
 
     @OneToMany(mappedBy = "qna", cascade = CascadeType.ALL)
     private final List<Reply> replyList = new ArrayList<>();
 
     private Qna(final Member writer, final String title, final String content) {
         super(writer, title, content);
+        this.status = Status.UN_RESPONSE;
     }
 
     public static Qna create(final Member writer, final String title, final String content) {
         return new Qna(writer, title, content);
+    }
+
+    public void addImage(final QnaImage qnaImage){
+        images.add(qnaImage);
     }
 
     public void addReply(final Reply reply){
@@ -60,4 +69,5 @@ public class Qna extends BaseContent {
     private void incrementReplyCnt() {
         replyCnt++;
     }
+
 }
--- src/main/java/com/travelers/biz/domain/Reply.java
+++ src/main/java/com/travelers/biz/domain/Reply.java
@@ -1,6 +1,7 @@
 package com.travelers.biz.domain;
 
 import com.travelers.biz.domain.base.BaseContent;
+import com.travelers.biz.domain.base.BaseTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Reply extends BaseContent {
+public class Reply extends BaseTime {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,13 +22,24 @@ public class Reply extends BaseContent {
     @JoinColumn(name = "qna_id")
     private Qna qna;
 
-    public Reply(final Member writer, final String title, final String content, final Qna qna){
-        super(writer, title, content);
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member writer;
+
+    private String content;
+
+    public Reply(final Member writer, final String content, final Qna qna){
+        this.writer = writer;
+        this.content = content;
         relationWith(qna);
     }
 
     private void relationWith(final Qna qna) {
         this.qna = qna;
         qna.addReply(this);
+    }
+
+    public void edit(final String content) {
+        this.content = content;
     }
 }
