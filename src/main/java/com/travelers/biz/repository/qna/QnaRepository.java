4b95b1795a56543bc06c03014f706ebd9659e0e0
jungsunghun <jsh951227@gmail.com>
2022-10-03 16:14:59 +0900

    feat: querydsl custom 클래스 상속

--- src/main/java/com/travelers/biz/repository/qna/QnaRepository.java
+++ src/main/java/com/travelers/biz/repository/qna/QnaRepository.java
@@ -3,5 +3,5 @@ package com.travelers.biz.repository.qna;
 import com.travelers.biz.domain.Qna;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QnaRepository extends JpaRepository<Qna, Long> {
+public interface QnaRepository extends JpaRepository<Qna, Long>, QnaRepositoryQuery{
 }
