55461c2815cc9a7a8d5d5491f90aa1c1ca9a1261
jungsunghun <jsh951227@gmail.com>
2022-10-03 16:12:43 +0900

    feat: 1:1 문의 페이징 로직

--- src/main/java/com/travelers/biz/repository/qna/QnaRepositoryImpl.java
+++ src/main/java/com/travelers/biz/repository/qna/QnaRepositoryImpl.java
@@ -1,4 +1,36 @@
 package com.travelers.biz.repository.qna;
 
-public class QnaRepositoryImpl implements QnaRepositoryQuery{
+import com.travelers.biz.domain.QQna;
+import com.travelers.biz.repository.notify.config.QuerydslSupports;
+import com.travelers.dto.QQnaResponse_SimpleList;
+import com.travelers.dto.QnaResponse;
+import com.travelers.dto.paging.PagingCorrespondence;
+import org.springframework.data.domain.Pageable;
+
+import javax.persistence.EntityManager;
+
+import static com.travelers.biz.domain.QQna.qna;
+
+public class QnaRepositoryImpl extends QuerydslSupports implements QnaRepositoryQuery {
+
+    public QnaRepositoryImpl(EntityManager em) {
+        super(em);
+    }
+
+    public PagingCorrespondence.Response<QnaResponse.SimpleList> findSimpleList(final Pageable pageable) {
+        return PagingCorrespondence.Response.from(
+                applyPagination(pageable,
+                        () -> select(new QQnaResponse_SimpleList(
+                                qna.id,
+                                qna.status,
+                                qna.title,
+                                qna.createdAt
+                        ))
+                                .from(qna)
+                                .offset(pageable.getOffset())
+                                .limit(pageable.getPageSize())
+                                .orderBy(qna.id.desc()),
+                        select(qna.count()).from(qna)
+                ));
+    }
 }
--- src/main/java/com/travelers/biz/repository/qna/QnaRepositoryQuery.java
+++ src/main/java/com/travelers/biz/repository/qna/QnaRepositoryQuery.java
@@ -1,4 +1,9 @@
 package com.travelers.biz.repository.qna;
 
+import com.travelers.dto.QnaResponse;
+import com.travelers.dto.paging.PagingCorrespondence;
+import org.springframework.data.domain.Pageable;
+
 public interface QnaRepositoryQuery {
+    PagingCorrespondence.Response<QnaResponse.SimpleList> findSimpleList(final Pageable pageable);
 }
