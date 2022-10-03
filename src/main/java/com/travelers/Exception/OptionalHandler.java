d19e0b66eb9b123505b849dbf3ea17cb5429121f
jungsunghun <jsh951227@gmail.com>
2022-10-03 15:50:51 +0900

    feat: 공통 예외 처리를 위한 handler 추가

---
+++ src/main/java/com/travelers/Exception/OptionalHandler.java
@@ -0,0 +1,17 @@
+package com.travelers.exception;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class OptionalHandler {
+
+    public static <T> T findMember(Supplier<Optional<T>> supplier){
+        return supplier.get()
+                .orElseThrow(() -> new TravelersException(ErrorCode.MEMBER_NOT_FOUND));
+    }
+
+    public static <T> T findWithNotfound(Supplier<Optional<T>> supplier){
+        return supplier.get()
+                .orElseThrow(() -> new TravelersException(ErrorCode.RESOURCE_NOT_FOUND));
+    }
+}
