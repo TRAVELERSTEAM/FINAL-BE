22e3c5c0b73dc5394ce974bdf473ca9b69a59492
jungsunghun <jsh951227@gmail.com>
2022-10-04 21:27:43 +0900

    feat: 사용하지 않는 코드 삭제 및 코드 수정

--- src/main/java/com/travelers/biz/repository/TravelPlaceRepository.java
+++ src/main/java/com/travelers/biz/repository/TravelPlaceRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface TravelPlaceRepository extends JpaRepository<TravelPlace, Long> {
 
-    @Query("select count(tp.id) < 1 from TravelPlace tp where tp.member.id = :memberId and tp.product.id = :productId")
+    @Query("select count(tp.id) > 0 from TravelPlace tp where tp.member.id = :memberId and tp.product.id = :productId")
     boolean existsPlace(@Param("memberId") final Long memberId, @Param("productId") final Long productId);
 }
