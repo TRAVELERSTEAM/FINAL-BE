8a979bbbb23eb9e998bfdaa77d88a7387f1b1b8c
jungsunghun <jsh951227@gmail.com>
2022-10-04 21:38:43 +0900

    feat: product entity 추가

---
+++ src/main/java/com/travelers/biz/domain/Product.java
@@ -0,0 +1,19 @@
+package com.travelers.biz.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
