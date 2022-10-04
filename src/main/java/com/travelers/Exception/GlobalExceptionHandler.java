ca5209b8509e2e6d1be32cb7ee9275cdff252af6
jungsunghun <jsh951227@gmail.com>
2022-10-04 14:10:26 +0900

    feat: 컨트롤러로 잘못된 파라미터 값이 넘어왔을 때 처리 로직

---
+++ src/main/java/com/travelers/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.travelers.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static com.travelers.exception.ErrorCode.DUPLICATE_RESOURCE;
+
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { ConstraintViolationException.class, DataIntegrityViolationException.class})
+    protected ResponseEntity<ErrorResponseDto> handleDataException() {
+        log.error("handleDataException throw Exception : {}", DUPLICATE_RESOURCE);
+        return ErrorResponseDto.toResponseEntity(DUPLICATE_RESOURCE);
+    }
+
+    @ExceptionHandler(value = { TravelersException.class })
+    protected ResponseEntity<ErrorResponseDto> handleCustomException(TravelersException e) {
+        log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
+        return ErrorResponseDto.toResponseEntity(e.getErrorCode());
+    }
+
+    @ExceptionHandler(value = { IllegalStateException.class } )
+    public ResponseEntity<String> handlerException() {
+        return ResponseEntity.badRequest().body("잘못된 요청 입니다.");
+    }
+}
