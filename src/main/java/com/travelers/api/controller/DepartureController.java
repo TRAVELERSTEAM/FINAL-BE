c2777fa5415736423bce00e7a2443375016a989f
jungsunghun <jsh951227@gmail.com>
2022-10-05 22:04:29 +0900

    feat: 상품별 예약 가능 목록 조회 로직

---
+++ src/main/java/com/travelers/api/controller/DepartureController.java
@@ -0,0 +1,28 @@
+package com.travelers.api.controller;
+
+import com.travelers.biz.service.DepartureService;
+import com.travelers.dto.DepartureResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/departure")
+public class DepartureController {
+
+    private final DepartureService departureService;
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<List<DepartureResponse.TravelPeriod>> getList(
+            @PathVariable final Long productId
+    ) {
+        return ResponseEntity.ok(departureService.availableReservationList(productId));
+    }
+
+}
