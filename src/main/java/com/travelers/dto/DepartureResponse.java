2a612f472835189cd36967b8428cb13c6d771204
jungsunghun <jsh951227@gmail.com>
2022-10-06 02:12:46 +0900

    feat: 예약 화면에 맞춘 dto 생성

--- src/main/java/com/travelers/dto/DepartureResponse.java
+++ src/main/java/com/travelers/dto/DepartureResponse.java
@@ -1,9 +1,12 @@
 package com.travelers.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
+import com.travelers.biz.domain.departure.embeddable.Price;
+import com.travelers.biz.domain.reservation.embeddable.Capacity;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class DepartureResponse {
 
@@ -22,6 +25,30 @@ public class DepartureResponse {
             this.departureId = departureId;
             this.whenDeparture = whenDeparture;
             this.whenReturn = whenReturn;
+        }
+    }
+
+    @Getter
+    public static class ReservationInfo {
+        private final Long id;
+        private final String productName;
+        private final Period period;
+        private final Price price;
+        private final Capacity capacity;
+
+        @QueryProjection
+        public ReservationInfo(
+                final Long id,
+                final String productName,
+                final Period period,
+                final Price price,
+                final Capacity capacity
+        ) {
+            this.id = id;
+            this.productName = productName;
+            this.period = period;
+            this.price = price;
+            this.capacity = capacity;
         }
     }
 }
