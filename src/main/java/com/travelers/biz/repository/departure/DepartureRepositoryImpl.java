331f33d253fa1d16139711483947b46c9e229843
jungsunghun <jsh951227@gmail.com>
2022-10-05 16:29:44 +0900

    feat: 예약 가능한 날짜 목록 출력 쿼리 작성

--- src/main/java/com/travelers/biz/repository/departure/DepartureRepositoryImpl.java
+++ src/main/java/com/travelers/biz/repository/departure/DepartureRepositoryImpl.java
@@ -1,12 +1,29 @@
 package com.travelers.biz.repository.departure;
 
+import com.travelers.biz.domain.departure.Departure;
+import com.travelers.biz.domain.departure.QDeparture;
 import com.travelers.biz.repository.notify.config.QuerydslSupports;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.travelers.biz.domain.departure.QDeparture.departure;
 
 public class DepartureRepositoryImpl extends QuerydslSupports implements DepartureRepositoryQuery {
 
+    private static final QDeparture D = departure;
+
     protected DepartureRepositoryImpl(EntityManager em) {
         super(em);
+    }
+
+    @Override
+    public List<LocalDate> findAllByProductId(Long productId) {
+        return select(D.whenDeparture)
+                .from(D)
+                .where(D.product.id.eq(productId)
+                        .and(D.status.eq(Departure.Status.OPENED)))
+                .fetch();
     }
 }
--- src/main/java/com/travelers/biz/repository/departure/DepartureRepositoryQuery.java
+++ src/main/java/com/travelers/biz/repository/departure/DepartureRepositoryQuery.java
@@ -1,4 +1,9 @@
 package com.travelers.biz.repository.departure;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface DepartureRepositoryQuery {
+
+    List<LocalDate> findAllByProductId(final Long productId);
 }
