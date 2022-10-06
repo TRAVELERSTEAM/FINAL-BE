9969ecbd6e2aeb1889b52ec705f7a6cbec3272e5
jungsunghun <jsh951227@gmail.com>
2022-10-06 19:26:51 +0900

    ease enter the commit message for your changes. Lines starting

---
+++ src/main/java/com/travelers/admin/controller/AdminDepartureController.java
@@ -0,0 +1,32 @@
+package com.travelers.admin.controller;
+
+import com.travelers.biz.service.DepartureService;
+import com.travelers.dto.DepartureRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/departure")
+public class AdminDepartureController {
+
+    private final DepartureService departureService;
+
+    @PostMapping
+    public ResponseEntity<Void> create(
+            final Long productId,
+            final DepartureRequest.Create createReq
+    ) {
+        departureService.createDeparture(productId, createReq);
+        return ResponseEntity.ok().build();
+    }
+    
+    @DeleteMapping("/{departureId}")
+    public ResponseEntity<Void> delete(
+            final Long departureId
+    ) {
+        departureService.delete(departureId);
+        return ResponseEntity.noContent().build();
+    }
+}
