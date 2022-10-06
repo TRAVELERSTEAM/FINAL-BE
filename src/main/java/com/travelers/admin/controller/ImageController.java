01bbaefc6ccafecbe01454f976fd84a22297c8f8
moner050 <minhyung0237@gmail.com>
2022-10-06 21:20:48 +0900

    feat: Add @RequestPart in uploadImage

--- src/main/java/com/travelers/admin/controller/ImageController.java
+++ src/main/java/com/travelers/admin/controller/ImageController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,8 +21,8 @@ public class ImageController {
     private final ImageService imageService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<List<String>> uploadNotifyImage(
-            final List<MultipartFile> files
+    public ResponseEntity<List<String>> uploadImage(
+            @RequestPart(value = "files") final List<MultipartFile> files
     ){
         return ResponseEntity.ok(imageService.uploadImage(SecurityUtil.getCurrentMemberId(), files));
     }
