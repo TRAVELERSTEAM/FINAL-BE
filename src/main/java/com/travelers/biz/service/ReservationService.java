d53f79e00aaf614211e3e66df55f765db54e74ff
jungsunghun <jsh951227@gmail.com>
2022-10-06 18:21:56 +0900

    feat: 예약 목록 조회 로직 작성

--- src/main/java/com/travelers/biz/service/ReservationService.java
+++ src/main/java/com/travelers/biz/service/ReservationService.java
@@ -5,12 +5,14 @@ import com.travelers.biz.domain.departure.Departure;
 import com.travelers.biz.domain.reservation.Reservation;
 import com.travelers.biz.domain.reservation.embeddable.HeadCount;
 import com.travelers.biz.repository.MemberRepository;
-import com.travelers.biz.repository.ReservationRepository;
+import com.travelers.biz.repository.reservation.ReservationRepository;
 import com.travelers.biz.repository.departure.DepartureRepository;
+import com.travelers.dto.ReservationResInfo;
+import com.travelers.dto.paging.PagingCorrespondence;
 import com.travelers.exception.ErrorCode;
-import com.travelers.exception.TravelersException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.travelers.exception.OptionalHandler.*;
 
@@ -22,6 +24,7 @@ public class ReservationService {
     private final DepartureRepository departureRepository;
     private final ReservationRepository reservationRepository;
 
+    @Transactional
     public void create(
             final Long memberId,
             final Long departureId,
@@ -32,5 +35,13 @@ public class ReservationService {
         final Reservation reserve = departure.reserve(member, headCount);
 
         reservationRepository.save(reserve);
+    }
+
+    @Transactional(readOnly = true)
+    public PagingCorrespondence.Response<ReservationResInfo> reservationList(
+            final Long memberId,
+            final PagingCorrespondence.Request paging
+    ) {
+        return reservationRepository.findListByMemberId(memberId, paging.toPageable());
     }
 }
