8711fa42c54dc3bc8b3c7fcbe347f3c39c57f8fe
jungsunghun <jsh951227@gmail.com>
2022-10-06 19:44:11 +0900

    feat: 회원과 예약 entity의 키 값으로 Reservation Entity 찾는 로직 작성

--- src/main/java/com/travelers/biz/repository/reservation/ReservationRepository.java
+++ src/main/java/com/travelers/biz/repository/reservation/ReservationRepository.java
@@ -2,6 +2,13 @@ package com.travelers.biz.repository.reservation;
 
 import com.travelers.biz.domain.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long>, ReservationRepositoryQuery {
+
+    @Query(" select r from Reservation r where r.id = :reservationId and r.member.id = :memberId")
+    Optional<Reservation> findByIdAndMemberId(@Param("reservationId") final Long reservationId, @Param("memberId") final Long memberId);
 }
