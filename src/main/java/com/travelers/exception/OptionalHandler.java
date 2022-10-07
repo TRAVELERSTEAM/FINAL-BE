562e4ef9ec143820172b3888592a11bf65a1283f
jungsunghun <jsh951227@gmail.com>
2022-10-07 17:27:38 +0900

    feat: @Deprecated 추가 및 리팩토링

--- src/main/java/com/travelers/exception/OptionalHandler.java
+++ src/main/java/com/travelers/exception/OptionalHandler.java
@@ -7,33 +7,44 @@ import java.util.function.Supplier;
 
 public class OptionalHandler {
 
-    public static <T> T findMember(Supplier<Optional<T>> supplier){
+    @Deprecated
+    public static <T> T findMember(Supplier<Optional<T>> supplier) {
         return supplier.get()
                 .orElseThrow(() -> new TravelersException(ErrorCode.MEMBER_NOT_FOUND));
     }
 
-    public static <T> T findWithNotfound(Supplier<Optional<T>> supplier){
+    @Deprecated
+    public static <T> T findWithNotfound(Supplier<Optional<T>> supplier) {
         return supplier.get()
                 .orElseThrow(() -> new TravelersException(ErrorCode.RESOURCE_NOT_FOUND));
     }
 
-    public static <T> T findProduct(Supplier<Optional<T>> supplier){
+    @Deprecated
+    public static <T> T findProduct(Supplier<Optional<T>> supplier) {
         return supplier.get()
                 .orElseThrow(() -> new TravelersException(ErrorCode.PRODUCT_NOT_FOUND));
     }
 
+    @Deprecated
     public static <T> T findReview(Supplier<Optional<T>> supplier) {
         return supplier.get()
                 .orElseThrow(() -> new TravelersException(ErrorCode.RESOURCE_NOT_FOUND));
     }
 
-    public static <T> T findOrThrow(Long id, Function<Long, Optional<T>> function, ErrorCode errorCode){
-        return function.apply(id)
-                .orElseThrow(() -> new TravelersException(errorCode));
+    public static <T, R> R findOrThrow(T identifier, Function<T, Optional<R>> function, ErrorCode errorCode) {
+        return orElseThrow(() -> function.apply(identifier), errorCode);
     }
 
-    public static <T> T findOrThrow(final Long id1, final Long id2, BiFunction<Long, Long, Optional<T>> function, ErrorCode errorCode){
-        return function.apply(id1, id2)
+    public static <T, R> R findOrThrow(final T identifier1, final T identifier2, BiFunction<T, T, Optional<R>> biFunction, ErrorCode errorCode) {
+        return orElseThrow(() -> biFunction.apply(identifier1, identifier2), errorCode);
+    }
+
+    public static <T, U, R> R findBiOrThrow(final T identifier1, final U identifier2, BiFunction<T, U, Optional<R>> biFunction, ErrorCode errorCode) {
+        return orElseThrow(() -> biFunction.apply(identifier1, identifier2), errorCode);
+    }
+
+    private static <R> R orElseThrow(Supplier<Optional<R>> supplier, ErrorCode errorCode) {
+        return supplier.get()
                 .orElseThrow(() -> new TravelersException(errorCode));
     }
 }
