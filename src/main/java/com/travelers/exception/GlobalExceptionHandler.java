ab67a44e1f9ef9bef0452e5322434bae4fedb1dc
jungsunghun <jsh951227@gmail.com>
2022-10-08 00:33:27 +0900

    feat: @Valid reject value 처리 로직

--- src/main/java/com/travelers/exception/GlobalExceptionHandler.java
+++ src/main/java/com/travelers/exception/GlobalExceptionHandler.java
@@ -1,12 +1,18 @@
 package com.travelers.exception;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.travelers.exception.ErrorCode.DUPLICATE_RESOURCE;
 
@@ -21,7 +27,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(value = { TravelersException.class })
-    protected ResponseEntity<ErrorResponseDto> handleCustomException(TravelersException e) {
+    protected ResponseEntity<ErrorResponseDto> handleCustomException(final TravelersException e) {
         log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
         return ErrorResponseDto.toResponseEntity(e.getErrorCode());
     }
@@ -29,5 +35,23 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = { IllegalStateException.class } )
     public ResponseEntity<String> handlerException() {
         return ResponseEntity.badRequest().body("잘못된 요청 입니다.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> validHandling(final BindingResult br) {
+
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse(br));
+    }
+
+    @Getter
+    final static class ErrorResponse  {
+        private final Map<String, Object> rejectPair = new HashMap<>();
+
+        ErrorResponse(final BindingResult bindingResult) {
+            bindingResult.getFieldErrors()
+                    .forEach(e -> rejectPair.put(e.getField(), e.getRejectedValue()));
+        }
     }
 }
