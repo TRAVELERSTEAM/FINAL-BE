98a5fd89f6923fb4ebcede50d33223497519c35c
jungsunghun <jsh951227@gmail.com>
2022-10-08 04:05:30 +0900

    fix

--- src/main/java/com/travelers/exception/GlobalExceptionHandler.java
+++ src/main/java/com/travelers/exception/GlobalExceptionHandler.java
@@ -4,14 +4,19 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.travelers.exception.ErrorCode.DUPLICATE_RESOURCE;
@@ -39,12 +44,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .body("잘못된 요청 값 입니다.");
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> validHandling(final BindingResult br) {
-
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid (
+            final MethodArgumentNotValidException ex,
+            final HttpHeaders headers,
+            final HttpStatus status,
+            final WebRequest request
+    ) {
         return ResponseEntity
                 .badRequest()
-                .body(new ErrorResponse(br));
+                .body(new ErrorResponse(ex.getBindingResult()));
     }
 
     @Getter
