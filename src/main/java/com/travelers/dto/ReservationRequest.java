81005d9040a3640f56acfc219ee397de520b7dc1
jungsunghun <jsh951227@gmail.com>
2022-10-10 21:50:57 +0900

    feat: Member entity 변환 로직 추가

--- src/main/java/com/travelers/dto/ReservationRequest.java
+++ src/main/java/com/travelers/dto/ReservationRequest.java
@@ -1,8 +1,36 @@
 package com.travelers.dto;
 
-public class ReservationReq {
+import com.travelers.annotation.Tel;
+import com.travelers.biz.domain.Member;
+import com.travelers.biz.domain.reservation.embeddable.HeadCount;
+import lombok.Getter;
 
-    public static class Create {
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
+public class ReservationRequest {
+
+    @Getter
+    public static class Anonymous {
+        @NotNull
+        private String name;
+
+        @Email
+        private String email;
+
+        @Tel
+        private String tel;
+
+        @Valid
+        private HeadCount headCount;
+
+        public Member toAnonymousMember() {
+            return Member.builder()
+                    .username(name)
+                    .email(email)
+                    .tel(tel)
+                    .build();
+        }
     }
 }
