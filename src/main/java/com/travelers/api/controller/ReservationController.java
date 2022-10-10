3f2f1c2b359bbaf83024258b29535b7041351301
jungsunghun <jsh951227@gmail.com>
2022-10-10 21:52:09 +0900

    feat: 비회원 예약 로직 작성

--- src/main/java/com/travelers/api/controller/ReservationController.java
+++ src/main/java/com/travelers/api/controller/ReservationController.java
@@ -2,6 +2,7 @@ package com.travelers.api.controller;
 
 import com.travelers.biz.domain.reservation.embeddable.HeadCount;
 import com.travelers.biz.service.ReservationService;
+import com.travelers.dto.ReservationRequest;
 import com.travelers.dto.ReservationResInfo;
 import com.travelers.dto.paging.PagingCorrespondence;
 import com.travelers.util.SecurityUtil;
@@ -30,7 +31,16 @@ public class ReservationController {
             @PathVariable final Long departureId,
             @RequestBody final HeadCount headCount
     ) {
-        reservationService.create(currentMemberId(), departureId, headCount);
+        reservationService.memberCreate(currentMemberId(), departureId, headCount);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{departureId}/anonymous")
+    public ResponseEntity<Void> createNonMember(
+            @PathVariable final Long departureId,
+            @RequestBody final ReservationRequest.Anonymous headCount
+    ) {
+        reservationService.nonMemberCreate(departureId, headCount);
         return ResponseEntity.ok().build();
     }
 
