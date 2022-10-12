6962b0148f71806cb729d001c4e1234f84bf6484
moner050 <minhyung0237@gmail.com>
2022-10-13 03:03:33 +0900

    feat: add allBanner, oneBanner

--- src/main/java/com/travelers/admin/controller/AdminBannerController.java
+++ src/main/java/com/travelers/admin/controller/AdminBannerController.java
@@ -1,12 +1,15 @@
 package com.travelers.admin.controller;
 
 import com.travelers.biz.service.BannerService;
+import com.travelers.dto.BannerResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/admin")
@@ -14,9 +17,19 @@ import java.io.IOException;
 public class AdminBannerController {
     private final BannerService bannerService;
 
-    @PostMapping("/banner/load")
-    public void loadData() throws IOException {
+    @GetMapping("/banner/load")
+    public ResponseEntity<Objects> loadData() throws IOException {
         bannerService.loadData();
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @GetMapping("/banners")
+    public ResponseEntity<List<BannerResponseDto>> allBanner() {
+        return new ResponseEntity<>(bannerService.findAllBanner(), HttpStatus.OK);
+    }
+
+    @GetMapping("/banner/{bannerId}")
+    public ResponseEntity<BannerResponseDto> oneBanner(@PathVariable Long bannerId) {
+        return new ResponseEntity<>(bannerService.findBanner(bannerId), HttpStatus.OK);
+    }
 }
